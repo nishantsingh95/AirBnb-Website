f295dca50c5321995badbc7aa7cf85747079bf04
Nishant Singh <nishantsingh8195@gmail.com>
2026-01-03 14:32:29 +0530

    Fix: Restore missing function declaration in db.js

--- Airbnb/backend/config/db.js
+++ Airbnb/backend/config/db.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-try {
-    await mongoose.connect(process.env.MONGODB_URL || process.env.MONGO_URI, {
-        serverSelectionTimeoutMS: 5000
-    })
-    console.log("DB connected")
-} catch (error) {
-    console.log("db error", error)
-    throw error;
-}
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL || process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 5000
+        })
+        console.log("DB connected")
+    } catch (error) {
+        console.log("db error", error)
+        throw error;
+    }
 }
 export default connectDb
